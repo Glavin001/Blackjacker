ba0f03cf26ab8e1ec38ac681c5ac89cfeaf09053
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-03-12 00:52:00 -0300

    Removed Android boilerplate code and setup basic foundation from UML diagram.

---
+++ Blackjacker/src/Dealer.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author glavin
+ *
+ */
+public class Dealer extends Player {
+	
+	private int minimumHandValue;
+	private Deck deck;
+	private ArrayList<Player> players;
+	private Player currentPlayer;
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Player getNextPlayer()
+	{
+		// TODO
+		return null;
+	}
+	
+}
